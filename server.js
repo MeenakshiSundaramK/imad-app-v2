64696be8054314dfb51a549a37aec93bf17ef909
MeenakshiSundaramK <MeenakshiSundaramK@users.noreply.github.com>
2017-02-13 20:44:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,24 +5,64 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleone = {
-    Title : 'Article One | By MSK',
-    Heading : 'Article One',
-    date : 'Feb 11, 2017',
-    content : `
-                <p>
-                    This is my first article.This is my first article.This is my first article.
-                    This is my first article.This is my first article.This is my first article.
-                </p>
-                <p>
-                    This is my first article.This is my first article.This is my first article.
-                    This is my first article.This is my first article.This is my first article.
-                </p>
-                <p>
-                    This is my first article.This is my first article.This is my first article.
-                    This is my first article.This is my first article.This is my first article.
-                </p>`
-
+var articles = {  
+    'article-one' : {
+        Title : 'Article One | By MSK',
+        Heading : 'Article One',
+        date : 'Feb 11, 2017',
+        content : `
+                    <p>
+                        This is my first article.This is my first article.This is my first article.
+                        This is my first article.This is my first article.This is my first article.
+                    </p>
+                    <p>
+                        This is my first article.This is my first article.This is my first article.
+                        This is my first article.This is my first article.This is my first article.
+                    </p>
+                    <p>
+                        This is my first article.This is my first article.This is my first article.
+                        This is my first article.This is my first article.This is my first article.
+                    </p>`
+    
+    },
+    'article-two' : {
+        Title : 'Article Two | By MSK',
+        Heading : 'Article Two',
+        date : 'Feb 11, 2017',
+        content : `
+                    <p>
+                        This is my first article.This is my first article.This is my first article.
+                        This is my first article.This is my first article.This is my first article.
+                    </p>
+                    <p>
+                        This is my first article.This is my first article.This is my first article.
+                        This is my first article.This is my first article.This is my first article.
+                    </p>
+                    <p>
+                        This is my first article.This is my first article.This is my first article.
+                        This is my first article.This is my first article.This is my first article.
+                    </p>`
+    
+    },
+    'article-three' : {
+        Title : 'Article Three | By MSK',
+        Heading : 'Article Three',
+        date : 'Feb 11, 2017',
+        content : `
+                    <p>
+                        This is my first article.This is my first article.This is my first article.
+                        This is my first article.This is my first article.This is my first article.
+                    </p>
+                    <p>
+                        This is my first article.This is my first article.This is my first article.
+                        This is my first article.This is my first article.This is my first article.
+                    </p>
+                    <p>
+                        This is my first article.This is my first article.This is my first article.
+                        This is my first article.This is my first article.This is my first article.
+                    </p>`
+    
+    }
 };
 
 function createtemplate(data) {
@@ -67,8 +107,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.send(createtemplate(articleone));
+app.get('/:articlename', function (req, res) {
+  var articlename = rec.params.articlename;
+  res.send(createtemplate(articles[articlename]));
 });
 
 app.get('/article-two', function (req, res) {
