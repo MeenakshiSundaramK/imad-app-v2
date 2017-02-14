ae2002685865d2ee72dbdd485e5c27b0a3a2957c
MeenakshiSundaramK <MeenakshiSundaramK@users.noreply.github.com>
2017-02-14 08:44:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,6 +11,6 @@ function moveright()
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function() {
-    'img.style.marginLeft='100px';  
+
     var interval = setinterval(moveright,100);
 };
