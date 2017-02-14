a618baa27bc7bc0ec32e86e8775997416948ed6b
MeenakshiSundaramK <MeenakshiSundaramK@users.noreply.github.com>
2017-02-14 08:44:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,13 @@ var element = document.getElementById('main-text');
 element.innerHTML='New Version';
 
 var img = document.getElementById('madi');
+var marginleft = 0;
+function moveright()
+{
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function() {
-    img.style.marginLeft='100px';  
+    'img.style.marginLeft='100px';  
+    var interval = setinterval(moveright,100);
 };
