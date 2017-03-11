c306dfb507f2894f56bd4570d9029f988f9b12d4
MeenakshiSundaramK <MeenakshiSundaramK@users.noreply.github.com>
2017-03-11 07:48:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.get('/ui/madi.png', function (req, res) {
 
 function hash(input, salt) {
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed.tostring('hex');
+    return hashed.toString('hex');
 }
 app.get('/hash/:input',function(req,res) {
     var hashedString = hash(req.params.input, 'random-string');
