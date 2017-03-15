040fbf79beadc541b21ddfb2137cd39fee9a8a98
MeenakshiSundaramK <MeenakshiSundaramK@users.noreply.github.com>
2017-03-15 07:54:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -137,6 +137,31 @@ app.post('/create-user', function(req, res) {
    });
 });
 
+app.post('/login', function(req, res) {
+   var username = req.body.username;
+   var password = req.body.password;
+
+   pool.query(' SELECT * FROM "user" WHERE username = $1',[username],function(err,result) {
+       if (err) {
+           res.status(500).send(err.toString());
+       }
+       else {
+           if (result.row.length === 0) {
+               res.send(403).send('Username/password is invalid');
+           } else {
+               var dbString = result.rows[0].password;
+               var salt = dbString.split('$')[2];
+               var hashedPassword = hash(password, salt);
+               if (hashedPassword === password) {
+                   res.send("Credentials correct");
+               } else {
+                   res.send(403).send('Username/password is invalid');
+               }
+           }
+           res.send("User successfully created:", + username);
+       }
+    });
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
